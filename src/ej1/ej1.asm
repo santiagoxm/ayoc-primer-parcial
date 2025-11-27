df8eed8512927e5a2852421bc25a7c4df1cac673
Santiago Moreno <santiagoxaviermoreno@gmail.com>
2025-11-27 10:25:27 -0300

    ej1

--- src/ej1/ej1.asm
+++ src/ej1/ej1.asm
@@ -1,4 +1,5 @@
 extern malloc
+extern strcpy
 
 ;########### SECCION DE DATOS
 section .data
@@ -9,29 +10,100 @@ section .text
 
 ; Completar las definiciones (serán revisadas por ABI enforcer):
 TUIT_MENSAJE_OFFSET EQU 0
-TUIT_FAVORITOS_OFFSET EQU 0
-TUIT_RETUITS_OFFSET EQU 0
-TUIT_ID_AUTOR_OFFSET EQU 0
-TUIT_SIZE EQU 0
+TUIT_FAVORITOS_OFFSET EQU 140
+TUIT_RETUITS_OFFSET EQU 142
+TUIT_ID_AUTOR_OFFSET EQU 144
+TUIT_SIZE EQU 148
 
 PUBLICACION_NEXT_OFFSET EQU 0
-PUBLICACION_VALUE_OFFSET EQU 0
-PUBLICACION_SIZE EQU 0
+PUBLICACION_VALUE_OFFSET EQU 8
+PUBLICACION_SIZE EQU 16
 
 FEED_FIRST_OFFSET EQU 0 
-FEED_SIZE EQU 0
+FEED_SIZE EQU 8
 
 USUARIO_FEED_OFFSET EQU 0;
-USUARIO_SEGUIDORES_OFFSET EQU 0; 
-USUARIO_CANT_SEGUIDORES_OFFSET EQU 0; 
-USUARIO_SEGUIDOS_OFFSET EQU 0; 
-USUARIO_CANT_SEGUIDOS_OFFSET EQU 0; 
-USUARIO_BLOQUEADOS_OFFSET EQU 0; 
-USUARIO_CANT_BLOQUEADOS_OFFSET EQU 0; 
-USUARIO_ID_OFFSET EQU 0; 
-USUARIO_SIZE EQU 0
+USUARIO_SEGUIDORES_OFFSET EQU 8; 
+USUARIO_CANT_SEGUIDORES_OFFSET EQU 16; 
+USUARIO_SEGUIDOS_OFFSET EQU 24; 
+USUARIO_CANT_SEGUIDOS_OFFSET EQU 32; 
+USUARIO_BLOQUEADOS_OFFSET EQU 40; 
+USUARIO_CANT_BLOQUEADOS_OFFSET EQU 48; 
+USUARIO_ID_OFFSET EQU 52; 
+USUARIO_SIZE EQU 56
 
 ; tuit_t *publicar(char *mensaje, usuario_t *usuario);
 global publicar
 publicar:
-ret
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+
+	mov r12, rdi ; char mensaje*
+	mov r13, rsi ; usuario_t usuario*
+	
+	mov rdi, TUIT_SIZE
+	call malloc
+	mov r14, rax ; tuit_t*
+
+	mov rdi, r14
+	mov rsi, r12
+	call strcpy
+
+	mov word [r14 + TUIT_FAVORITOS_OFFSET], 0
+	mov word [r14 + TUIT_RETUITS_OFFSET], 0
+	mov r10d, dword [r13 + USUARIO_ID_OFFSET]
+	mov dword [r14 + TUIT_ID_AUTOR_OFFSET], r10d
+
+	mov rdi, r14
+	mov rsi, qword [r13 + USUARIO_FEED_OFFSET]
+
+	call agregarTuitAFeed
+
+	xor r15, r15 ; i
+	mov rbx, [r13 + USUARIO_SEGUIDORES_OFFSET]
+	.iterarSobreSeguidores:
+	cmp r15d, dword [r13 + USUARIO_CANT_SEGUIDORES_OFFSET]
+	je .return
+	mov r11, qword [rbx + r15*8]
+	mov rdi, r14 
+	mov rsi, qword [r11 + USUARIO_FEED_OFFSET]
+	call agregarTuitAFeed
+	inc r15d
+	jmp .iterarSobreSeguidores
+
+	.return:
+
+	mov rax, r14 
+
+	pop rbx
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	ret
+
+agregarTuitAFeed:
+	push r12
+	push r13
+	push r14
+
+	mov r12, rdi ; tweet_t
+	mov r13, rsi ; feed_t
+
+	mov rdi, PUBLICACION_SIZE
+	call malloc
+	mov r14, rax ; publicacion_t
+
+	mov qword [r14 + PUBLICACION_VALUE_OFFSET], r12
+
+	mov r10, qword [r13 + FEED_FIRST_OFFSET]
+	mov qword [r14 + PUBLICACION_NEXT_OFFSET], r10
+	mov qword [r13 + FEED_FIRST_OFFSET], r14
+
+	pop r14
+	pop r13
+	pop r12
+	ret
--- src/ej1/ej1.c
+++ src/ej1/ej1.c
@@ -1,9 +1,36 @@
 #include "../ejs.h"
 #include <string.h>
+#include <stdlib.h>
+
+
+void publicarEnFeed(tuit_t* tuit, feed_t* feed) {
+
+	publicacion_t* publicacion = malloc(16);
+	publicacion->next = feed->first;
+	publicacion->value = tuit;
+
+	feed->first = publicacion;
+
+	return;
+
+}
 
 // Función principal: publicar un tuit
-tuit_t *publicar(char *mensaje, usuario_t *user) {
+tuit_t* publicar(char *mensaje, usuario_t *user) {
 
-  return NULL;
-  
+	tuit_t* tuit = malloc(160);
+	strcpy((char*)&tuit->mensaje, mensaje);
+	tuit->favoritos = 0;
+	tuit->retuits = 0;
+	tuit->id_autor = user->id;
+
+	publicarEnFeed(tuit, user->feed);
+
+	for (unsigned int i = 0; i < user->cantSeguidores; i++) {
+
+		publicarEnFeed(tuit, user->seguidores[i]->feed);
+
+	}
+
+	return tuit;
 }
